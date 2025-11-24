7e7c40eedf5f9b1bd38cedd2ed322d42341ed3c5
dnii0412 <123633815+dnii0412@users.noreply.github.com>
2025-11-24 12:20:55 +0800

    main look: done

---
+++ app.js
@@ -0,0 +1,10 @@
+// choices
+// win condition if bot or player wins?
+// display win points each wins
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
+
+let playerScore = document.getElementById("player-point");
+let botScore = document.getElementById("bot-point");
